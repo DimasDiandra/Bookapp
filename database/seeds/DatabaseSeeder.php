9aaf275f71a19edc8f7e15d3645c08d7afc2b46a
unknown <dimasdiandra9@gmail.com>
2020-11-09 16:31:34 +0700

    message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,38 @@ class DatabaseSeeder extends Seeder
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
+
+        DB::table('author')->insert([
+            'name' => 'Ali Rusli',
+            'gender' => 'Male',
+            'biography' => 'Ali Rusli merupakan penulis muda yang saat ini namanya sedang naik daun. Berkat karyanya yang menyentuh, Ali Rusli mampu menjangkau selera semua kalangan masyarakat pada umumnya.
+
+            Ali Rusli merupakan anak terakhir dari pasangan Ardiansyah dengan Maria yang merupakan seorang mualaf berbangsaan China. Ali Rusli memiliki dua kakak perempuan bernama Casandra dan Clara.
+            
+            Ayah Rusli merupakan dosen di Universitas Indonesia, sedangkan ibunya adalah seorang jurnalistik untuk portal berita. Ali Rusli dan keluarga sangat menyukai seni tulis, bahkan kedua kakaknya juga berprofesi sebagai penulis novel.
+            
+            Hidup dalam keluarga yang memiliki ketertarikan dibidang jurnalistik membuat Ali juga menyukai bidang tersebut. Tak heran jika sejak kecil Ali berkeinginan untuk mengikuti jejak keluarganya untuk menjadi seorang penulis.
+            
+            Untuk memperdalam pemahaman Ali dalam bidang jurnalistik, akhirnya ia memutuskan untuk mengambil kuliah jurusan Sastra di Universitas Padjajaran. Selama masa kuliah Ali sudah mulai menulis diberbagai media online.
+            
+            Ketertarikan Ali dalam bidang penulisan cerpen dan novel membuat ia akhirnya menyelesaikan karya pertamanya. Novel tersebut ia kirimkan ke penerbit dan ternyata reaksinya sangat luar biasa.
+            
+            Melalui novel berjudul “Pulang” Ali memulai debutnya sebagai penulis. Karyanya yang menginspirasi membuat banyak sekali orang yang menyukainya.',
+
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('author')->insert([
+            'name' => 'Mira Alzahra',
+            'gender' => 'Female',
+            'biography' => 'Senja merupakan nama pena yang dimiliki oleh sebuah penulis bernama asli Mira Alzahra. Bisa dibilang bahwa Mira merupakan salah satu penulis yang berbakat dan paling produktif.
+
+            Hal tersebut terbukti dari berbagai hasil karyanya yang muncul pada setiap tahun. Karya yang dihasilkan Mira cukup beragam mulai dari kisah romansa hingga kalimat-kalimat yang menginspirasi. Beberapa karya Mira bahkan masuk dalam jajaran novel best seller yang banyak dicari oleh para pembaca. Selain menerbitkan buku, Mira juga masih aktif menulis cerpen di beberapa media online.',
+
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
         // $this->call('UsersTableSeeder');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,13 @@ $router->get('/key', function () {
 });
 $router->get('books', 'BooksController@index');
 $router->get('books/{id}', 'BooksController@id');
-
 $router->post('books', 'BooksController@store');
 $router->put('books/{id}', 'BooksController@update');
 $router->delete('books/{id}', 'BooksController@destroy');
+
+
+$router->get('author', 'AuthorController@index');
+$router->get('author/{id}', 'AuthorController@id');
+$router->post('author', 'AuthorController@store');
+$router->put('author/{id}', 'AuthorController@update');
+$router->delete('author/{id}', 'AuthorController@destroy');
